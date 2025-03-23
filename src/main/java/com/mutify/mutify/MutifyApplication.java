d2539f0369ef57ab49bc726cc4f9cd3e945a6072
nnam-droid12 <williamekene700@gmail.com>
2025-03-23 16:18:38 +0100

    empty spring boot application

---
+++ src/main/java/com/mutify/mutify/MutifyApplication.java
@@ -0,0 +1,13 @@
+package com.mutify.mutify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mutify/mutify/MutifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutify.mutify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
